d396d37dc3decc2e41b98d1b762852b1e5c8a018
yunyl <32681723+heyimyunyl@users.noreply.github.com>
2021-03-20 22:55:25 +0300

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,2 +1,20 @@
-node main.js
+@shift /0
+SetLocal EnableDelayedExpansion
+$global:ProgressPreference = 'SilentlyContinue'
+
+@echo off
+SET version=1.2.0
+TITLE tapeSerializer2
+mode con: cols=100 lines=32
+color 0D
+goto :startScreen
+
+
+:startScreen
+echo.
+echo Welcome to tapeSerializer2 by Yunyl.
+echo.
+SET /p mapName="Please enter the codename of the song in the input folder: "
+echo.
+node main.js %mapName%
 pause
